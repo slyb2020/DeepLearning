51c2a0ba1f5d4cbd083159227c9a4b62a183fbe0
slyb2020 <slyb@tju.edu.cn>
2022-01-19 18:15:20 +0800

    2022年1月19日18:15提交,实现具有归一化处理功能的二维线性回归类。

--- LinearRegression2D.py
+++ LinearRegression2D.py
@@ -1,7 +1,10 @@
-#当使用未归一化数据时，由于输入数据没有归一化处理，所以学习率只能取得非常小，在取0.000016时，需要迭代1564个epoch才能得到较好的解。
-#当使用归一化数据时，由于输入数据有归一化处理，所以学习率可以较大，在取1时，甚至在第0个epoch中即可求得非常好的解。
-#可见归一化对于缩短训练时间的效果非常显著
-#20220119 11:44在ZenBook上增加归一化处理功能
+# 当使用未归一化数据时，由于输入数据没有归一化处理，所以学习率只能取得非常小，在取0.000016时，需要迭代1564个epoch才能得到较好的解。
+# 当使用归一化数据时，由于输入数据有归一化处理，所以学习率可以较大，在取1时，甚至在第0个epoch中即可求得非常好的解。
+# 可见归一化对于缩短训练时间的效果非常显著
+# 20220119 11:44在ZenBook上增加归一化处理功能
+# 针对data_Regression_unregular.csv中的数据，如果不使用归一化处理，learningRate需要设置为0.00001，而且还需要经过2915个epoch的迭代
+# 才能得到一个比较准确的结果；如果使用归一化处理，则learningRate可以简单的设置为1，只需要1个epoch就能得到较为理想的结果.
+# 可见归一化处理对于提高线性回归收敛速度具有非常显著的效果！
 import numpy as np
 import pandas as pd
 
@@ -18,13 +21,26 @@ def OpenDataFile(fileName):
 
 
 class LinearRegression2D:
-    def __init__(self, dimension=2, learningRate=1, maxLoss=1e-10, epoch=100000):
+    def __init__(self, dimension=2, learningRate=1, threshold=1e-10, max_epochs=100000,regularization=False):
         self.dimension = dimension
         self.learningRate = learningRate
-        self.maxLoss = maxLoss
-        self.epoch = epoch
+        self.threshold = threshold
+        self.max_epochs = max_epochs
+        self.regularization = regularization
         self.omega = np.array([0.0] * self.dimension)
         self.bias = 0.0
+
+    def Regularization(self,inputX,labels):
+        self.minX = inputX.min(0)
+        maxX = inputX.max(0)
+        self.rangeX = maxX - self.minX
+        print("minX=",self.minX)
+        inputX = (inputX-self.minX)/self.rangeX
+        return inputX,labels
+
+    def UnRegularization(self):
+        self.bias = self.bias-np.dot(self.omega*self.minX/self.rangeX,np.array([1]*self.dimension))
+        self.omega = self.omega/self.rangeX
 
     def Predict(self, x):
         y = np.dot(x, self.omega) + self.bias
@@ -36,23 +52,29 @@ class LinearRegression2D:
         return loss
 
     def Fit(self, inputX, labels):
-        for i in range(self.epoch):
+        if self.regularization:
+            inputX,labels = self.Regularization(inputX,labels)
+        for i in range(self.max_epochs):
             for j, x in enumerate(inputX):
                 y = self.Predict(x)
                 error = labels[j] - y
                 loss = self.Loss(y, labels[j])
-                if loss <= self.maxLoss:
+                if loss <= self.threshold:
+                    if self.regularization:
+                        self.UnRegularization()
                     return loss, i
                 self.omega += self.learningRate * x * error
                 self.bias += self.learningRate * error
-        return loss, self.epoch
+        if self.regularization:
+            self.UnRegularization()
+        return loss, self.max_epochs
 
 
 if __name__ == '__main__':
-    # filename = linearRegressionDataDir + 'LinearRegression2D.csv'
-    filename = linearRegressionDataDir + 'data_Regression_regular.csv'
+    filename = linearRegressionDataDir + 'data_Regression_unregular.csv'
+    # filename = linearRegressionDataDir + 'data_Regression_regular.csv'
     xVectors, labels = OpenDataFile(filename)
-    myLinearRegression2D = LinearRegression2D()
+    myLinearRegression2D = LinearRegression2D(regularization=True)
     loss, epoch = myLinearRegression2D.Fit(xVectors, labels)
     print("loss,epoch=", loss, epoch)
     print("omega=", myLinearRegression2D.omega)
