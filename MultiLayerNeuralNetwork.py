0cdbab13513f276e1cd2d2fa6922699b3f76c508
slyb2020 <slyb@tju.edu.cn>
2022-01-21 22:54:27 +0800

    2022年1月21日22:53提交,新建Dev分支,开始实现MultiLayerNeuralNetwork类

--- MultiLayerNeuralNetwork.py
+++ MultiLayerNeuralNetwork.py
@@ -1,4 +1,5 @@
 import numpy as np
+from BatchGenerator import BatchGenerator
 
 
 def Relu(inputVectors):
@@ -7,12 +8,44 @@ def Relu(inputVectors):
 
 
 class MultyLayerNeuralNetwork:
-    def __init__(self, inputShape, shape, activators):
+    def __init__(self, inputShape, shape, activators, threshold=1e-5, maxEpoch=1000, regularization=True,
+                 miniBatchSize=50, momentum=0.9):
+        if len(activators) != len(shape):
+            raise Exception("激活函数数量与神经网络层数不匹配，程序无法运行！请在下次运行程序前确保为每层网络都指定激活函数！")
+        self.inputShape = inputShape
+        self.shape = shape
+        self.treshold = threshold
+        self.maxEpoch = maxEpoch
+        self.regularization = regularization
+        self.miniBatchSize = miniBatchSize
+        self.momentum = momentum
+
+        self.depth = len(shape)
+        self.activityLevels = [np.mat(0)] * self.depth
+
+        self.weights = [np.mat(0)] * self.depth
+        self.bias = [np.mat(0)] * self.depth
+
+        self.weights[0] = np.mat(np.random.random((shape[0],inputShape))/100.)
+        self.bias[0] = np.mat(np.random.random((shape[0],1))/100.)
+        for i,idx in enumerate(self.shape[1:]):
+            self.weights[i+1] = np.mat(np.random.random((idx, self.shape[i]))/100.)
+            self.bias[i+1] = np.mat(np.random.random((idx,1))/100.)
+
         self.activatorFunc = []
         self.activatorFuncDiff = []
         for activator in activators:
             if activator.upper() == 'RELU':
                 self.activatorFunc.append(np.vectorize(self.Relu))
+
+    def Fit(self, inputX, labels):
+        i=0
+        batchGenerator = BatchGenerator([inputX, labels], self.miniBatchSize, shuffle=True)
+        for [xBatch,labelBatch],epoch in batchGenerator:
+            print("xBatch,labelBatch,epoch=",xBatch,labelBatch,epoch)
+            i+=1
+            if i>=3:
+                break
 
     @staticmethod
     def Relu(x):
@@ -23,10 +56,13 @@ class MultyLayerNeuralNetwork:
         return 1.0 if x > 0 else 0.0
 
 
-
 if __name__ == '__main__':
-    myMultyLayerNeuralNetwork = MultyLayerNeuralNetwork(3,3,['relu'])
-    inputVectors = np.random.rand(10, 1) - 0.5
-    print("input", inputVectors)
-    outputVectors = myMultyLayerNeuralNetwork.activatorFunc[0](inputVectors)
-    print("output", outputVectors)
+    myMultyLayerNeuralNetwork = MultyLayerNeuralNetwork(2, [8,4], ['relu','relu'])
+    # print("weights=",myMultyLayerNeuralNetwork.weights)
+    # print("bias=",myMultyLayerNeuralNetwork.bias)
+    inputVectors = np.random.rand(100, 2) - 0.5
+    labels = np.random.random((100,1))
+    # print("input", inputVectors)
+    # outputVectors = myMultyLayerNeuralNetwork.activatorFunc[0](inputVectors)
+    # print("output", outputVectors)
+    myMultyLayerNeuralNetwork.Fit(inputVectors,labels)
