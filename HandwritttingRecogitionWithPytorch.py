3153d4667d54623cc25186c899ea455fbcfd3552
slyb2020 <slyb@tju.edu.cn>
2022-01-28 15:08:49 +0800

    2022年1月27日17:01在ZenBook上提交,初步完成基于Pytorch的手写数字识别

--- HandwritttingRecogitionWithPytorch.py
+++ HandwritttingRecogitionWithPytorch.py
@@ -5,9 +5,11 @@ import numpy as np
 from torch import nn
 from torchvision.datasets import MNIST
 import matplotlib.pyplot as plt
+from torchvision import transforms
+from torch.utils.data import DataLoader
 # %matplotlib inline
-mnist = MNIST(root='D:/WorkSpace/DataSet', train=True, download=True)
 def MyImageShow():
+    mnist = MNIST(root='D:/WorkSpace/DataSet', train=True, download=True)
     for i, j in enumerate(np.random.randint(0, len(mnist),(10,))):
         data, label = mnist[j]
         plt.subplot(2, 5, i+1)
@@ -17,11 +19,12 @@ def MyImageShow():
 class MLP(nn.Module):
     def __init__(self):
         super().__init__()
-        self.inputLayer = nn.Sequential(nn.Linear(28*28,256), nn.ReLU(), nn.Dropout(0.2))
-        self.hiddenLayer = nn.Sequential(nn.Linear(256,256), nn.ReLU(), nn.Dropout(0.2))
-        self.outputLayer = nn.Sequential(nn.Linear(256,10))
+        self.inputLayer = nn.Sequential(nn.Linear(28*28, 256), nn.ReLU(), nn.Dropout(0.2))
+        self.hiddenLayer = nn.Sequential(nn.Linear(256, 256), nn.ReLU(), nn.Dropout(0.2))
+        self.outputLayer = nn.Sequential(nn.Linear(256, 10))
 
     def forward(self, x):
+        x = x.view(x.size(0), -1)
         x = self.inputLayer(x)
         x = self.hiddenLayer(x)
         x = self.outputLayer(x)
@@ -35,6 +38,35 @@ def accuracy(pred, target):
 
 
 if __name__ == "__main__":
-    MyImageShow()
+    # MyImageShow()
+    trans = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,),(0.3081,))
+    ])
+    mnistTrain = MNIST(root='D:/WorkSpace/DataSet', train=True, download=True, transform=trans)
+    mnistVal = MNIST(root="D:/WorkSpace/DataSet", train=False, download=True, transform=trans)
+    trainLoader = DataLoader(mnistTrain, batch_size=16, shuffle=True, num_workers=2)
+    valLoader = DataLoader(mnistVal, batch_size=16, shuffle=True, num_workers=2)
+
     myMLP = MLP()
     print(myMLP)
+    Optimizer = optim.SGD(myMLP.parameters(), lr=1e-2, momentum=0.9)
+    Loss = nn.CrossEntropyLoss()
+    minLoss = 0
+    ACC = {'train':[],"val":[]}
+    LOSS = {'train':[],'val':[]}
+    for epoch in tqdm(range(10)):
+        # myMLP.eval()  #设置为验证模式
+        myMLP.train()
+        numTr, denumerTr, lossSum = 0., 0., 0.,
+        for data, target in trainLoader:
+            Optimizer.zero_grad()  #梯度初始化为0
+            output = myMLP(data)
+            loss = Loss(output, target)
+            lossSum += loss
+            loss.backward()
+            Optimizer.step()
+            num,denum = accuracy(output,target)
+            numTr += num
+            denumerTr += denum
+        # LOSS['train'].append(loss)
