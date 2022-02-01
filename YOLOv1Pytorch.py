4d52ee05b30f49b4688c77461dfff37cd9d6f409
slyb2020 <slyb@tju.edu.cn>
2022-02-01 21:13:39 +0800

    2022年2月1日21:13在ASUS上提交,初步完成YOLOv1编程，开始使用CPU()进行训练

--- YOLOv1Pytorch.py
+++ YOLOv1Pytorch.py
@@ -14,6 +14,7 @@ CLASSES = ['person', 'bird', 'cat', 'cow', 'dog', 'horse', 'sheep',
            'aeroplane', 'bicycle', 'boat', 'bus', 'car', 'motorbike', 'train',
            'bottle', 'chair', 'dining table', 'potted plant', 'sofa', 'tvmonitor']
 
+
 def convert(size, box):
     """将bbox的左上角点、右下角点坐标的格式，转换为bbox中心点+bbox的w,h的格式
     并进行归一化"""
@@ -27,16 +28,16 @@ def convert(size, box):
     w = w * dw
     y = y * dh
     h = h * dh
-    return (x, y, w, h)
+    return x, y, w, h
 
 
 def convert_annotation(image_id):
     """把图像image_id的xml文件转换为目标检测的label文件(txt)
     其中包含物体的类别，bbox的左上角点坐标以及bbox的宽、高
     并将四个物理量归一化"""
-    in_file = open(DATASET_PATH + 'Annotations/%s' % (image_id))
+    in_file = open(DATASET_PATH + 'Annotations/%s' % image_id)
     image_id = image_id.split('.')[0]
-    out_file = open('./labels/%s.txt' % (image_id), 'w')
+    out_file = open('./labels/%s.txt' % image_id, 'w')
     tree = ET.parse(in_file)
     root = tree.getroot()
     size = root.find('size')
@@ -51,7 +52,7 @@ def convert_annotation(image_id):
         cls_id = CLASSES.index(cls)
         xmlbox = obj.find('bndbox')
         points = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text),
-             float(xmlbox.find('ymax').text))
+                  float(xmlbox.find('ymax').text))
         bb = convert((w, h), points)
         out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
 
@@ -62,34 +63,36 @@ def make_label_txt():
     for file in filenames:
         convert_annotation(file)
 
+
 def show_labels_img(imgname):
     """imgname是输入图像的名称，无下标"""
     img = cv2.imread(DATASET_PATH + "JPEGImages/" + imgname + ".jpg")
     h, w = img.shape[:2]
-    print(w,h)
+    print(w, h)
     label = []
-    with open("./labels/"+imgname+".txt",'r') as flabel:
+    with open("./labels/" + imgname + ".txt", 'r') as flabel:
         for label in flabel:
             label = label.split(' ')
             label = [float(x.strip()) for x in label]
             print(CLASSES[int(label[0])])
             pt1 = (int(label[1] * w - label[3] * w / 2), int(label[2] * h - label[4] * h / 2))
             pt2 = (int(label[1] * w + label[3] * w / 2), int(label[2] * h + label[4] * h / 2))
-            cv2.putText(img,CLASSES[int(label[0])],pt1,cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255))
-            cv2.rectangle(img,pt1,pt2,(0,0,255,2))
+            cv2.putText(img, CLASSES[int(label[0])], pt1, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+            cv2.rectangle(img, pt1, pt2, (0, 0, 255, 2))
 
-    cv2.imshow("img",img)
+    cv2.imshow("img", img)
     cv2.waitKey(0)
 
+
 class VOC2012(Dataset):
-    def __init__(self,is_train=True,is_aug=True):
+    def __init__(self, is_train=True, is_aug=True):
         """
         :param is_train: 调用的是训练集(True)，还是验证集(False)
         :param is_aug:  是否进行数据增广
         """
         self.filenames = []  # 储存数据集的文件名称
         if is_train:
-            with open(DATASET_PATH + "ImageSets/Main/train.txt", 'r') as f: # 调用包含训练集图像名称的txt文件
+            with open(DATASET_PATH + "ImageSets/Main/train.txt", 'r') as f:  # 调用包含训练集图像名称的txt文件
                 self.filenames = [x.strip() for x in f]
         else:
             with open(DATASET_PATH + "ImageSets/Main/val.txt", 'r') as f:
@@ -102,19 +105,19 @@ class VOC2012(Dataset):
         return len(self.filenames)
 
     def __getitem__(self, item):
-        img = cv2.imread(self.imgpath+self.filenames[item]+".jpg")  # 读取原始图像
-        h,w = img.shape[0:2]
+        img = cv2.imread(self.imgpath + self.filenames[item] + ".jpg")  # 读取原始图像
+        h, w = img.shape[0:2]
         input_size = 448  # 输入YOLOv1网络的图像尺寸为448x448
         # 因为数据集内原始图像的尺寸是不定的，所以需要进行适当的padding，将原始图像padding成宽高一致的正方形
         # 然后再将Padding后的正方形图像缩放成448x448
         padw, padh = 0, 0  # 要记录宽高方向的padding具体数值，因为padding之后需要调整bbox的位置信息
-        if h>w:
+        if h > w:
             padw = (h - w) // 2
-            img = np.pad(img,((0,0),(padw,padw),(0,0)),'constant',constant_values=0)
-        elif w>h:
+            img = np.pad(img, ((0, 0), (padw, padw), (0, 0)), 'constant', constant_values=0)
+        elif w > h:
             padh = (w - h) // 2
-            img = np.pad(img,((padh,padh),(0,0),(0,0)), 'constant', constant_values=0)
-        img = cv2.resize(img,(input_size,input_size))
+            img = np.pad(img, ((padh, padh), (0, 0), (0, 0)), 'constant', constant_values=0)
+        img = cv2.resize(img, (input_size, input_size))
         # 图像增广部分，这里不做过多处理，因为改变bbox信息还蛮麻烦的
         if self.is_aug:
             aug = transforms.Compose([
@@ -123,15 +126,15 @@ class VOC2012(Dataset):
             img = aug(img)
 
         # 读取图像对应的bbox信息，按1维的方式储存，每5个元素表示一个bbox的(cls,xc,yc,w,h)
-        with open(self.labelpath+self.filenames[item]+".txt") as f:
+        with open(self.labelpath + self.filenames[item] + ".txt") as f:
             bbox = f.read().split('\n')
         bbox = [x.split() for x in bbox]
         bbox = [float(x) for y in bbox for x in y]
-        if len(bbox)%5!=0:
-            raise ValueError("File:"+self.labelpath+self.filenames[item]+".txt"+"——bbox Extraction Error!")
+        if len(bbox) % 5 != 0:
+            raise ValueError("File:" + self.labelpath + self.filenames[item] + ".txt" + "——bbox Extraction Error!")
 
         # 根据padding、图像增广等操作，将原始的bbox数据转换为修改后图像的bbox数据
-        for i in range(len(bbox)//5):
+        for i in range(len(bbox) // 5):
             if padw != 0:
                 bbox[i * 5 + 1] = (bbox[i * 5 + 1] * w + padw) / h
                 bbox[i * 5 + 3] = (bbox[i * 5 + 3] * w) / h
@@ -143,28 +146,30 @@ class VOC2012(Dataset):
         labels = convert_bbox2labels(bbox)  # 将所有bbox的(cls,x,y,w,h)数据转换为训练时方便计算Loss的数据形式(7,7,5*B+cls_num)
         # 此处可以写代码验证一下，经过convert_bbox2labels函数后得到的labels变量中储存的数据是否正确
         labels = transforms.ToTensor()(labels)
-        return img,labels
+        return img, labels
+
 
 def convert_bbox2labels(bbox):
     """将bbox的(cls,x,y,w,h)数据转换为训练时方便计算Loss的数据形式(7,7,5*B+cls_num)
     注意，输入的bbox的信息是(xc,yc,w,h)格式的，转换为labels后，bbox的信息转换为了(px,py,w,h)格式"""
-    gridsize = 1.0/7
-    labels = np.zeros((7,7,5*NUM_BBOX+len(CLASSES)))  # 注意，此处需要根据不同数据集的类别个数进行修改
-    for i in range(len(bbox)//5):
-        gridx = int(bbox[i*5+1] // gridsize)  # 当前bbox中心落在第gridx个网格,列
-        gridy = int(bbox[i*5+2] // gridsize)  # 当前bbox中心落在第gridy个网格,行
+    gridsize = 1.0 / 7
+    labels = np.zeros((7, 7, 5 * NUM_BBOX + len(CLASSES)))  # 注意，此处需要根据不同数据集的类别个数进行修改
+    for i in range(len(bbox) // 5):
+        gridx = int(bbox[i * 5 + 1] // gridsize)  # 当前bbox中心落在第gridx个网格,列
+        gridy = int(bbox[i * 5 + 2] // gridsize)  # 当前bbox中心落在第gridy个网格,行
         # (bbox中心坐标 - 网格左上角点的坐标)/网格大小  ==> bbox中心点的相对位置
         gridpx = bbox[i * 5 + 1] / gridsize - gridx
         gridpy = bbox[i * 5 + 2] / gridsize - gridy
         # 将第gridy行，gridx列的网格设置为负责当前ground truth的预测，置信度和对应类别概率均置为1
         labels[gridy, gridx, 0:5] = np.array([gridpx, gridpy, bbox[i * 5 + 3], bbox[i * 5 + 4], 1])
         labels[gridy, gridx, 5:10] = np.array([gridpx, gridpy, bbox[i * 5 + 3], bbox[i * 5 + 4], 1])
-        labels[gridy, gridx, 10+int(bbox[i*5])] = 1
+        labels[gridy, gridx, 10 + int(bbox[i * 5])] = 1
     return labels
+
 
 class Loss_yolov1(nn.Module):
     def __init__(self):
-        super(Loss_yolov1,self).__init__()
+        super(Loss_yolov1, self).__init__()
 
     def forward(self, pred, labels):
         """
@@ -185,49 +190,57 @@ class Loss_yolov1(nn.Module):
         for i in range(n_batch):  # batchsize循环
             for n in range(7):  # x方向网格循环
                 for m in range(7):  # y方向网格循环
-                    if labels[i,4,m,n]==1:# 如果包含物体
+                    if labels[i, 4, m, n] == 1:  # 如果包含物体
                         # 将数据(px,py,w,h)转换为(x1,y1,x2,y2)
                         # 先将px,py转换为cx,cy，即相对网格的位置转换为标准化后实际的bbox中心位置cx,xy
                         # 然后再利用(cx-w/2,cy-h/2,cx+w/2,cy+h/2)转换为xyxy形式，用于计算iou
-                        bbox1_pred_xyxy = ((pred[i,0,m,n]+n)/num_gridx - pred[i,2,m,n]/2,(pred[i,1,m,n]+m)/num_gridy - pred[i,3,m,n]/2,
-                                           (pred[i,0,m,n]+n)/num_gridx + pred[i,2,m,n]/2,(pred[i,1,m,n]+m)/num_gridy + pred[i,3,m,n]/2)
-                        bbox2_pred_xyxy = ((pred[i,5,m,n]+n)/num_gridx - pred[i,7,m,n]/2,(pred[i,6,m,n]+m)/num_gridy - pred[i,8,m,n]/2,
-                                           (pred[i,5,m,n]+n)/num_gridx + pred[i,7,m,n]/2,(pred[i,6,m,n]+m)/num_gridy + pred[i,8,m,n]/2)
-                        bbox_gt_xyxy = ((labels[i,0,m,n]+n)/num_gridx - labels[i,2,m,n]/2,(labels[i,1,m,n]+m)/num_gridy - labels[i,3,m,n]/2,
-                                        (labels[i,0,m,n]+n)/num_gridx + labels[i,2,m,n]/2,(labels[i,1,m,n]+m)/num_gridy + labels[i,3,m,n]/2)
-                        iou1 = calculate_iou(bbox1_pred_xyxy,bbox_gt_xyxy)
-                        iou2 = calculate_iou(bbox2_pred_xyxy,bbox_gt_xyxy)
+                        bbox1_pred_xyxy = ((pred[i, 0, m, n] + n) / num_gridx - pred[i, 2, m, n] / 2,
+                                           (pred[i, 1, m, n] + m) / num_gridy - pred[i, 3, m, n] / 2,
+                                           (pred[i, 0, m, n] + n) / num_gridx + pred[i, 2, m, n] / 2,
+                                           (pred[i, 1, m, n] + m) / num_gridy + pred[i, 3, m, n] / 2)
+                        bbox2_pred_xyxy = ((pred[i, 5, m, n] + n) / num_gridx - pred[i, 7, m, n] / 2,
+                                           (pred[i, 6, m, n] + m) / num_gridy - pred[i, 8, m, n] / 2,
+                                           (pred[i, 5, m, n] + n) / num_gridx + pred[i, 7, m, n] / 2,
+                                           (pred[i, 6, m, n] + m) / num_gridy + pred[i, 8, m, n] / 2)
+                        bbox_gt_xyxy = ((labels[i, 0, m, n] + n) / num_gridx - labels[i, 2, m, n] / 2,
+                                        (labels[i, 1, m, n] + m) / num_gridy - labels[i, 3, m, n] / 2,
+                                        (labels[i, 0, m, n] + n) / num_gridx + labels[i, 2, m, n] / 2,
+                                        (labels[i, 1, m, n] + m) / num_gridy + labels[i, 3, m, n] / 2)
+                        iou1 = calculate_iou(bbox1_pred_xyxy, bbox_gt_xyxy)
+                        iou2 = calculate_iou(bbox2_pred_xyxy, bbox_gt_xyxy)
                         # 选择iou大的bbox作为负责物体
                         if iou1 >= iou2:
-                            coor_loss = coor_loss + 5 * (torch.sum((pred[i,0:2,m,n] - labels[i,0:2,m,n])**2) \
-                                        + torch.sum((pred[i,2:4,m,n].sqrt()-labels[i,2:4,m,n].sqrt())**2))
-                            obj_confi_loss = obj_confi_loss + (pred[i,4,m,n] - iou1)**2
+                            coor_loss = coor_loss + 5 * (
+                                        torch.sum((pred[i, 0:2, m, n] - labels[i, 0:2, m, n]) ** 2) + torch.sum(
+                                    (pred[i, 2:4, m, n].sqrt() - labels[i, 2:4, m, n].sqrt()) ** 2))
+                            obj_confi_loss = obj_confi_loss + (pred[i, 4, m, n] - iou1) ** 2
                             # iou比较小的bbox不负责预测物体，因此confidence loss算在noobj中，注意，对于标签的置信度应该是iou2
-                            noobj_confi_loss = noobj_confi_loss + 0.5 * ((pred[i,9,m,n]-iou2)**2)
+                            noobj_confi_loss = noobj_confi_loss + 0.5 * ((pred[i, 9, m, n] - iou2) ** 2)
                         else:
-                            coor_loss = coor_loss + 5 * (torch.sum((pred[i,5:7,m,n] - labels[i,5:7,m,n])**2) \
-                                        + torch.sum((pred[i,7:9,m,n].sqrt()-labels[i,7:9,m,n].sqrt())**2))
-                            obj_confi_loss = obj_confi_loss + (pred[i,9,m,n] - iou2)**2
+                            coor_loss = coor_loss + 5 * (torch.sum((pred[i, 5:7, m, n] - labels[i, 5:7, m, n]) ** 2) + torch.sum(
+                                        (pred[i, 7:9, m, n].sqrt() - labels[i, 7:9, m, n].sqrt()) ** 2))
+                            obj_confi_loss = obj_confi_loss + (pred[i, 9, m, n] - iou2) ** 2
                             # iou比较小的bbox不负责预测物体，因此confidence loss算在noobj中,注意，对于标签的置信度应该是iou1
-                            noobj_confi_loss = noobj_confi_loss + 0.5 * ((pred[i, 4, m, n]-iou1) ** 2)
-                        class_loss = class_loss + torch.sum((pred[i,10:,m,n] - labels[i,10:,m,n])**2)
+                            noobj_confi_loss = noobj_confi_loss + 0.5 * ((pred[i, 4, m, n] - iou1) ** 2)
+                        class_loss = class_loss + torch.sum((pred[i, 10:, m, n] - labels[i, 10:, m, n]) ** 2)
                     else:  # 如果不包含物体
-                        noobj_confi_loss = noobj_confi_loss + 0.5 * torch.sum(pred[i,[4,9],m,n]**2)
+                        noobj_confi_loss = noobj_confi_loss + 0.5 * torch.sum(pred[i, [4, 9], m, n] ** 2)
 
         loss = coor_loss + obj_confi_loss + noobj_confi_loss + class_loss
         # 此处可以写代码验证一下loss的大致计算是否正确，这个要验证起来比较麻烦，比较简洁的办法是，将输入的pred置为全1矩阵，再进行误差检查，会直观很多。
-        return loss/n_batch
+        return loss / n_batch
 
-def calculate_iou(bbox1,bbox2):
+
+def calculate_iou(bbox1, bbox2):
     """计算bbox1=(x1,y1,x2,y2)和bbox2=(x3,y3,x4,y4)两个bbox的iou"""
     intersect_bbox = [0., 0., 0., 0.]  # bbox1和bbox2的交集
-    if bbox1[2]<bbox2[0] or bbox1[0]>bbox2[2] or bbox1[3]<bbox2[1] or bbox1[1]>bbox2[3]:
+    if bbox1[2] < bbox2[0] or bbox1[0] > bbox2[2] or bbox1[3] < bbox2[1] or bbox1[1] > bbox2[3]:
         pass
     else:
-        intersect_bbox[0] = max(bbox1[0],bbox2[0])
-        intersect_bbox[1] = max(bbox1[1],bbox2[1])
-        intersect_bbox[2] = min(bbox1[2],bbox2[2])
-        intersect_bbox[3] = min(bbox1[3],bbox2[3])
+        intersect_bbox[0] = max(bbox1[0], bbox2[0])
+        intersect_bbox[1] = max(bbox1[1], bbox2[1])
+        intersect_bbox[2] = min(bbox1[2], bbox2[2])
+        intersect_bbox[3] = min(bbox1[3], bbox2[3])
 
     area1 = (bbox1[2] - bbox1[0]) * (bbox1[3] - bbox1[1])  # bbox1面积
     area2 = (bbox2[2] - bbox2[0]) * (bbox2[3] - bbox2[1])  # bbox2面积
@@ -236,11 +249,10 @@ def calculate_iou(bbox1,bbox2):
     # print(intersect_bbox)
     # input()
 
-    if area_intersect>0:
+    if area_intersect > 0:
         return area_intersect / (area1 + area2 - area_intersect)  # 计算iou
     else:
         return 0
-
 
 
 if __name__ == "__main__":
